c4d801ead897e103ad466fb93f4493f210cd599f
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2023-05-30 20:47:40 -0500

    Tarea completada

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -14,37 +14,54 @@ export class FormularioComponent {
 
   constructor(private datosFormularioService: DatosFormularioService, private router: Router) {}
 
+  validarCampos(): boolean {
+    return this.nombre.length > 0 && this.validarEmail();
+  }
+
+  validarEmail(): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(this.email);
+  }
+
   onSubmit() {
-    Swal.fire({
-      title: '¿Estás seguro de enviar el formulario?',
-      text: 'Una vez enviado, no podrás deshacer esta acción.',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Sí, enviar',
-      cancelButtonText: 'Cancelar'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        // Almacena los datos del formulario en el servicio
-        this.datosFormularioService.setFormData({
-          nombre: this.nombre,
-          email: this.email
-        });
+    if (this.validarCampos()) {
+      Swal.fire({
+        title: '¿Estás seguro de enviar el formulario?',
+        text: 'Una vez enviado, no podrás deshacer esta acción.',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, enviar',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // Almacena los datos del formulario en el servicio
+          this.datosFormularioService.setFormData({
+            nombre: this.nombre,
+            email: this.email
+          });
 
-        // Redirige al componente RecibirComponent con los datos del formulario como parámetros
-        this.router.navigate(['/recibir', this.nombre, this.email]);
+          // Redirige al componente RecibirComponent con los datos del formulario como parámetros
+          this.router.navigate(['/recibir', this.nombre, this.email]);
 
-        Swal.fire(
-          'Enviado',
-          'El formulario ha sido enviado exitosamente.',
-          'success'
-        );
-      } else if (result.dismiss === Swal.DismissReason.cancel) {
-        Swal.fire(
-          'Cancelado',
-          'El envío del formulario ha sido cancelado.',
-          'error'
-        );
-      }
-    });
+          Swal.fire(
+            'Enviado',
+            'El formulario ha sido enviado exitosamente.',
+            'success'
+          );
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          Swal.fire(
+            'Cancelado',
+            'El envío del formulario ha sido cancelado.',
+            'error'
+          );
+        }
+      });
+    } else {
+      Swal.fire(
+        'Error',
+        'Por favor, completa todos los campos correctamente.',
+        'error'
+      );
+    }
   }
 }
